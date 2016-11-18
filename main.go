d85fcfd941d9aff827b0bc3858c73cdc224d1d2e
Gabe Dalay <gdalay@quartethealth.com>
2016-11-18 16:23:57 -0500

    Updated main to execute /etc/hosts write

--- main.go
+++ main.go
@@ -1,32 +1,42 @@
 package main
 
 import (
-  "fmt"
-  "bufio"
-  "gopkg.in/yaml.v2"
-  "io/ioutil"
-  "os"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"gopkg.in/yaml.v2"
 )
 
 type Config struct {
-  Emails []string
+	Emails []string
 }
 
 func main() {
-  filename := os.Args[1]
-  var config Config
-  source, err := ioutil.ReadFile(filename)
-  if err != nil {
-    panic(err)
-  }
+	filename := os.Args[1]
+	var config Config
+	source, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
 
-  err  = yaml.Unmarshal(source, &config)
-  if err != nil {
-    panic(err)
-  }
-  // fmt.Println(config.Emails)
+	err = yaml.Unmarshal(source, &config)
+	if err != nil {
+		panic(err)
+	}
 
-  hostFileHandler, err2 := os.OpenFile("/etc/hosts", os.O_APPEND, 0666)
-  writer
+	hostFileHandler, err2 := os.OpenFile("/etc/hosts", os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err2 != nil {
+		panic(err2)
+	}
+	defer hostFileHandler.Close()
+
+	for _, email := range config.Emails {
+		s := fmt.Sprintf("216.35.221.76 %s\r\n", email)
+		_, er := hostFileHandler.WriteString(s)
+		if er != nil {
+			panic(er)
+		}
+	}
 
 }
